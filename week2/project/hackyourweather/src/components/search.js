9a0473964f1a822b34b9ebc09931a2f3edc9406c
Mouawya <mouawya88@gmail.com>
2021-09-08 13:18:03 +0200

    modified the weather app

--- week2/project/hackyourweather/src/components/search.js
+++ week2/project/hackyourweather/src/components/search.js
@@ -4,30 +4,38 @@ import WeatherCard from "../weather-info";
 const Search = () => {
   const [weatherData, setWeatherData] = useState([]);
   const [cityName, setCityName] = useState("");
+  const [isFetching, setIsFetching] = useState(false);
 
   const fetchApi = async (e) => {
     e.preventDefault();
-    const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`
-    );
-    const data = await response.json();
-    setWeatherData(data);
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`
+      );
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (err) {
+      console.log(err);
+    }
+
+    setIsFetching(true);
   };
 
   return (
     <div>
       <form>
         <input
+          className="search-box"
           type="text"
           placeholder="search city..."
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        <button value="submit" onClick={fetchApi}>
+        <button className="btn" value="submit" onClick={fetchApi}>
           Search
         </button>
       </form>
-      <WeatherCard city={weatherData} />
+      {isFetching && <WeatherCard city={weatherData} />}
     </div>
   );
 };
--- week2/project/hackyourweather/src/weather-info.js
+++ week2/project/hackyourweather/src/weather-info.js
@@ -1,20 +1,22 @@
 import React from "react";
 
 const WeatherCard = ({ city }) => {
-  return (
-    <div className="weather-card">
-      <h1>
-        {city.name}, {city.sys.country}
-      </h1>
-      <h2>{city.weather[0].main}</h2>
-      <h3>{city.weather[0].description}</h3>
-      <p>
-        min temp: {Math.floor(city.main.temp_min - 273.15)}° <br />
-        max temp: {Math.floor(city.main.temp_max - 273.15)}° <br />
-        location: {city.coord.lon}, {city.coord.lat}
-      </p>
-    </div>
-  );
+  if (city.name)
+    return (
+      <div className="weather-card">
+        <h1>
+          {city.name}, {city.sys.country}
+        </h1>
+        <h2>{city.weather[0].main}</h2>
+        <h3>{city.weather[0].description}</h3>
+        <p>
+          min temp: {Math.floor(city.main.temp_min - 273.15)}° <br />
+          max temp: {Math.floor(city.main.temp_max - 273.15)}° <br />
+          location: {city.coord.lon}, {city.coord.lat}
+        </p>
+      </div>
+    );
+  return <p className="message">City not found!</p>;
 };
 
 export default WeatherCard;
